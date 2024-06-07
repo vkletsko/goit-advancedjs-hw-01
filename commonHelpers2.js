f419337d0715ff97f27f9c5b5597da377369b1d5
vkletsko <v.kletsko@gmail.com>
2024-06-07 17:17:41 +0000

    Deploying to gh-pages from @ vkletsko/goit-advancedjs-hw-01@857506a043f6d95cfdb8a83ef999bc4657b2b03b 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as s,t as i}from"./assets/vendor-1e54b975.js";window.onload=()=>{const o="videoplayer-current-time",e=new s("vimeo-player");e.setColor("#ffb510");const r=JSON.parse(localStorage.getItem(o))||0,a=({seconds:n})=>{try{const t=JSON.stringify(Math.floor(n));localStorage.setItem(o,t)}catch(t){console.log(t.message)}};e.on("timeupdate",i(a,1e3)),e.setCurrentTime(r)};
+//# sourceMappingURL=commonHelpers2.js.map
